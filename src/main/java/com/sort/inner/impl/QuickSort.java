47a4daeda52e6a6df061f5024410653769881fd1
shitian <shitian_mail@163.com>
2018-05-09 18:11:00 +0800

    算法

--- src/main/java/com/sort/inner/impl/QuickSort.java
+++ src/main/java/com/sort/inner/impl/QuickSort.java
@@ -7,6 +7,8 @@ package com.sort.inner.impl;
 
 import com.sort.inner.NumberSort;
 
+import java.util.Arrays;
+
 /**
  * 〈快速排序〉
  *
@@ -16,6 +18,13 @@ import com.sort.inner.NumberSort;
  */
 public class QuickSort implements NumberSort {
 
+    /**
+     * 1)默认以第一个元素为基准
+     * 2)比基准小的都放左边，大的都放右边，相同可以放左，也可以放右，自己定义[相同的不处理会导致死循环]
+     * 3)此时，基准元素将数组分为两个部分
+     * 4)采用分治，递归，最终实现有序
+     * @param elements
+     */
     @Override
     public void sort(int[] elements) {
         quickSort(elements, 0, elements.length - 1);
@@ -25,7 +34,6 @@ public class QuickSort implements NumberSort {
     private void quickSort(int[] elements, int low, int high) {
         if (low < high) {
             int middle = getMiddle(elements, low, high);
-            System.out.println(middle + "_" + low + "_" + high);
             quickSort(elements, low, middle - 1);
             quickSort(elements, middle + 1, high);
         }
@@ -34,7 +42,8 @@ public class QuickSort implements NumberSort {
     private int getMiddle(int[] elements, int low, int high) {
         int temp = elements[low];   //第一个元素为中轴
         while (low < high) {
-            while (low < high && temp < elements[high]) {
+            //一定要加=，否则数组中出现两个数字相同就会导致死循环
+            while (low < high && temp <= elements[high]) {
                 high--;
             }
             elements[low] = elements[high];
--- src/test/java/SortMain.java
+++ src/test/java/SortMain.java
@@ -21,7 +21,7 @@ public class SortMain {
 
 
     public static void main(String[] args) {
-        int[] nums = new int[]{5, 6, 3, 4, 6, 8, 9, 0, 1};
+        int[] nums = new int[]{5, 7, 4, 4, 6, 8, 9, 0, 1};
         System.out.println("排序前");
         System.out.println(Arrays.toString(nums));
 
